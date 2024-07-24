a91f83a73d5f5c48133a467af10c55698906c7a2
Komal Prajapat <work.technotoil@gmail.com>
2024-07-24 12:27:55 +0530

    update

--- src/Pages/AuthenticationManager/Index.jsx
+++ src/Pages/AuthenticationManager/Index.jsx
@@ -16,7 +16,7 @@ const AuthenticationManager = () => {
     >
       <Row className="justify-content-evenly container mt-5">
         {/* Column 1 */}
-        <Col xs={12} sm={8} md={8} lg={3} className="mb-3 boxes">
+        <Col xs={12} sm={10} md={8} lg={3} className="mb-3 boxes">
           <div className="text-center">
             <img src={create} alt="" />
             <h3 className="mt-2">Create</h3>
@@ -36,7 +36,7 @@ const AuthenticationManager = () => {
         </Col>
 
         {/* Column 3 */}
-        <Col xs={12} sm={8} md={8} lg={3} className="mb-3 boxes">
+        <Col xs={12} sm={10} md={8} lg={3} className="mb-3 boxes">
           <div className="text-center">
             <img src={manage} alt="" />
             <h3 className="mt-2">Manage</h3>
