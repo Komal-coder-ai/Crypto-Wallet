7355a1872ff3e92dd616dc7640a6636810a0b7f6
Komal-Prajapat <komalprajapat267@gmail.com>
2024-09-13 11:43:57 +0530

    update

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -1,102 +1,128 @@
 import React, { useState } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText, Button, Grid } from '@mui/material';
 import { HashLink } from 'react-router-hash-link';
-import ButtonCom from '../button/index';
-import Home from '../../Pages/home/Index';
+import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../../assets/logo4.png';
+import './index.css'; // Your custom styles
 import AuthenticationManager from '../../Pages/AuthenticationManager/Index';
 import Platform from '../../Pages/Platform';
 import Service from '../../Pages/services/Index';
-import Buyandsell from '../../Pages/Buyandsell';
-import headerbg from '../../assets/ss/headerbg.png';
-import './index.css';
-import logo from '../../assets/logo4.png';
 import Investing from '../../Pages/Investing';
-import fullbg from '../../assets/ss/fullbg.png';
-import { TfiAlignJustify } from "react-icons/tfi";
-import ReceiveTransmission from '../../Pages/Receivetransmissions/Index';
-
+import Buyandsell from '../../Pages/Buyandsell';
+import Home from '../../Pages/home/Index';
+import ButtonCom from '../button';
+import { Nav } from 'react-bootstrap';
+import { MarginRounded } from '@mui/icons-material';
 const NavbarComponent = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setMenuOpen(open);
   };
 
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
+  const menuItems = [
+    { label: 'Buy / Sell', to: '#BuySell' },
+    { label: 'Grow', to: '#Grow' },
+    { label: 'Markets', to: '#Markets' },
+    { label: 'Business', to: '#Business' },
+    { label: 'Support', to: '#Support' },
+  ];
 
   return (
     <>
-      <Navbar expand="lg" className="navbar">
+      <AppBar position="static" className="navbar" sx={{ background: 'none' }}>
+        <Toolbar>
+          <Grid container alignItems="center" justifyContent="space-between" sx={{ width: '90vw' }}>
+            {/* Logo Section */}
+            <Grid item xs={6} sm={4} md={2} container justifyContent="flex-start">
+              <IconButton edge="start" color="inherit" aria-label="logo" href="/" sx={{ padding: 0 }}>
+                <img src={logo} alt="Logo" style={{ width: '100px', height: 'auto' }} />
+              </IconButton>
+            </Grid>
 
-        <Navbar.Brand href="/" className="d-flex 
-        align-items-center mix-blend-multiply 
-        justify-content-center
-        " style={{ marginLeft: "50px" }}>
-          <img
-            src={logo}
-            alt="Logo"
-            style={{
-              width: '100px', height: 'auto',
+            {/* Menu Links Section */}
+            <Grid item xs={6} sm={8} md={6} className="d-flex justify-content-center">
+              <List className="navlink-container" sx={{ display: { xs: 'none', md: 'flex' }, justifyContent: 'space-evenly', width: '100%' }}>
+                {menuItems.map((item, index) => (
+                  <ListItem key={index} button component={HashLink} smooth to={item.to} className="nav_Link center_link">
+                    <ListItemText primary={item.label} />
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
 
-              // mixBlendMode: 'multiply',
+            {/* Buttons Section */}
+            <Grid item xs={6} sm={4} md={4}  container justifyContent="flex-end">
+              <Grid item sx={{ display: { xs: 'none', md: 'flex' } }}>
+                <Nav.Link className="nav_Link" as={HashLink} smooth to="#SignIn">
+                  <ButtonCom
+                    name="Sign In"
+                    bgcolor="none"
+                    border="1px solid"
+                  />
+                </Nav.Link>
+                <Nav.Link className="nav_Link" as={HashLink} smooth to="#Signup" >
+                  <ButtonCom
+                    name="Sign Up"
+                    bgcolor="linear-gradient(190deg, #18C8FF, #933FFE)"
+                    border="none"
+                
+                  />
+                </Nav.Link>
+              </Grid>
 
-            }} // Adjust the size as needed
-          />
 
 
-        </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav" className={`justify-content-center navbar-collapse ${menuOpen ? 'show' : ''}`}>
-          <Nav className=" d-flex justify-content-center navlinkcontinerfor" onClick={closeMenu}>
-            <Nav.Link className="nav_Link center_link" as={HashLink} smooth to="#BuySell">
-              Buy / Sell
-            </Nav.Link>
-            <Nav.Link className="nav_Link center_link" as={HashLink} smooth to="#Grow">
-              Grow
-            </Nav.Link>
-            <Nav.Link className="nav_Link center_link" as={HashLink} smooth to="#Markets">
-              Markets
-            </Nav.Link>
-            <Nav.Link className="nav_Link center_link" as={HashLink} smooth to="#Business">
-              Business
-            </Nav.Link>
-            <Nav.Link className="nav_Link center_link" as={HashLink} smooth to="#Support">
-              Support
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+            </Grid>
 
-        <Nav className="d-flex flex-row  buttoncontainer"
-          style={{
-            marginRight: "50px"
-          }}
+            <Grid item xs={6} sm={4} md={2} container >
+              <IconButton edge="end" color="inherit" aria-label="menu" onClick={toggleDrawer(true)} sx={{ display: { xs: 'block', md: 'none' } }}>
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
 
-        >
-          <Nav.Link className="nav_Link" as={HashLink} smooth to="#SignIn">
-            <ButtonCom name="Sign In" bgcolor="none" border="1px solid"></ButtonCom>
-          </Nav.Link>
-          <Nav.Link className="nav_Link" as={HashLink} smooth to="#Signup">
-            <ButtonCom name="Sign Up" bgcolor="linear-gradient(190deg, #18C8FF, #933FFE)" border="none"></ButtonCom>
-          </Nav.Link>
-        </Nav>
 
-        {/* Toggle icon (TfiAlignJustify) */}
-        <div className="toggle-icon">
-          <TfiAlignJustify
-            onClick={toggleMenu}
-            style={{
-              color: "white",
-              fontSize: "40px",
-              cursor: "pointer",
-              marginTop: "15px",
-              marginRight: "20px",
-              display: "block",
-            }}
-          />
-        </div>
-      </Navbar>
+      <Drawer anchor="right" open={menuOpen} onClose={toggleDrawer(false)}>
+        <List>
+          {menuItems.map((item, index) => (
+            <ListItem button key={index} component={HashLink} smooth to={item.to} onClick={toggleDrawer(false)}>
+              <ListItemText primary={item.label} />
+            </ListItem>
+          ))}
+          <ListItem button component={HashLink} smooth to="#SignIn" onClick={toggleDrawer(false)}>
+            <ListItemText primary="Sign In" />
+          </ListItem>
+          <ListItem button component={HashLink} smooth to="#Signup" onClick={toggleDrawer(false)}>
+            <ListItemText primary="Sign Up" />
+          </ListItem>
+        </List>
+      </Drawer>
 
+
+      {/* Drawer for Mobile Menu */}
+      <Drawer anchor="right" open={menuOpen} onClose={toggleDrawer(false)}>
+        <List>
+          {menuItems.map((item, index) => (
+            <ListItem button key={index} component={HashLink} smooth to={item.to} onClick={toggleDrawer(false)}>
+              <ListItemText primary={item.label} />
+            </ListItem>
+          ))}
+
+          {/* Move the Sign In / Sign Up buttons inside the drawer */}
+          <ListItem button component={HashLink} smooth to="#SignIn" onClick={toggleDrawer(false)}>
+            <ListItemText primary="Sign In" />
+          </ListItem>
+          <ListItem button component={HashLink} smooth to="#Signup" onClick={toggleDrawer(false)}>
+            <ListItemText primary="Sign Up" />
+          </ListItem>
+        </List>
+      </Drawer>
 
       <div>
         <div id="home">
